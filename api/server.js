a2f4259ea637cfd9626e3fa31a8d724026492a14
Francisco dos Passos <chico.81@hotmail.com>
2021-07-17 15:26:18 -0300

    New Features - Comments

--- api/server.js
+++ api/server.js
@@ -10,6 +10,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(multiparty());
 
+app.use(function (req, res, next) {
+
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+    res.setHeader("Access-Control-Allow-Headers", "content-type")
+    res.setHeader("Access-Control-Allow-Credentials", true)
+
+    next()
+})
+
 const port = 8080
 
 app.listen(port)
@@ -27,7 +37,6 @@ app.get('/', function (req, res) {
 
 // POST (create)
 app.post('/api', function (req, res) {
-    res.setHeader("Access-Control-Allow-Origin", "*")
 
     const date = new Date()
     time_stamp = date.getTime()
@@ -36,14 +45,14 @@ app.post('/api', function (req, res) {
 
     const path_origem = req.files.arquivo.path
     const path_destino = './uploads/' + url_imagem
-    
+
 
     fs.rename(path_origem, path_destino, function (err) {
         if (err) {
-            res.status(500).json({error: err})
+            res.status(500).json({ error: err })
             return
         }
-        
+
         const dados = {
             url_imagem: url_imagem,
             titulo: req.body.titulo
@@ -55,17 +64,16 @@ app.post('/api', function (req, res) {
                     if (err) {
                         res.json({ 'status': 'erro' })
                     } else
-                        res.json({'status' : 'inclusao realizada com sucesso'})
+                        res.json({ 'status': 'inclusao realizada com sucesso' })
                 })
                 mongoclient.close()
             })
         })
-    })   
+    })
 });
 
 // GET (ready)
 app.get('/api', function (req, res) {
-    res.setHeader("Access-Control-Allow-Origin", "*")
 
     db.open(function (err, mongoclient) {
         mongoclient.collection('postagens', function (err, collection) {
@@ -77,6 +85,21 @@ app.get('/api', function (req, res) {
             })
             mongoclient.close()
         })
+    })
+});
+
+app.get('/imagens/:imagem', function (req, res) {
+
+    var img = req.params.imagem
+
+    fs.readFile('./uploads/' + img, function (err, content) {
+        if (err) {
+            res.status(400).json(err)
+            return
+        }
+
+        res.writeHead(200, { 'content-type': 'image/jpg' })
+        res.end(content)
     })
 });
 
@@ -104,15 +127,24 @@ app.put('/api/:id', function (req, res) {
         mongoclient.collection('postagens', function (err, collection) {
             collection.update(
                 { _id: objectId(req.params.id) },
-                { $set: { titulo: req.body.titulo, url_imagem: req.body.url_imagem } },
+                {
+                    $push: {
+                        comentarios: {
+                            id_comentario: new objectId(),
+                            comentario: req.body.comentario
+                        }
+                    }
+                },
                 {},
                 function (err, records) {
                     if (err) {
                         res.json(err)
                     } else
                         res.json(records)
-                })
-            mongoclient.close()
+
+                    mongoclient.close()
+                }
+            )
         })
     })
 });
